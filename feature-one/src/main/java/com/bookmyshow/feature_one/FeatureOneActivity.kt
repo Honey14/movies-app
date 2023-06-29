d435cabd04a9367350da9b1ae205c1c13a7efb8c
Honey Sonwani <honeysonwani88@gmail.com>
2023-06-29 16:55:00 +0530

    Observe `FeatureOneViewModel` and update venue details in the UI

--- feature-one/src/main/java/com/bookmyshow/feature_one/FeatureOneActivity.kt
+++ feature-one/src/main/java/com/bookmyshow/feature_one/FeatureOneActivity.kt
@@ -1,15 +1,28 @@
 package com.bookmyshow.feature_one
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.bookmyshow.feature_one.di.FeatureOneDaggerProvider
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bookmyshow.feature_one.adapter.VenuesAdapter
+import com.bookmyshow.feature_one.databinding.ActivityFeatureOneBinding
+import com.bookmyshow.feature_one.model.Venue
 import com.bookmyshow.feature_one.viewmodel.FeatureOneViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FeatureOneActivity : AppCompatActivity() {
 
     private val viewModel: FeatureOneViewModel by viewModels<FeatureOneViewModel>()
     private lateinit var binding: ActivityFeatureOneBinding
+    private lateinit var venuesAdapter: VenuesAdapter
+
     private val listOfVenues
         get() = binding.listOfVenues
 
@@ -19,5 +32,81 @@ class FeatureOneActivity : AppCompatActivity() {
 
         binding = ActivityFeatureOneBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        filterIfImageClicked()
+
+
+        if (isInternetPermissionGranted()) {
+            showVenueList()
+        } else {
+            requestInternetPermission()
+        }
+    }
+
+    private fun filterIfImageClicked() {
+        var venueListObserver: Observer<List<Venue>>? = null
+        venueListObserver = if (imagefilterVenues.drawable == ContextCompat.getDrawable(
+                this,
+                R.drawable.filter_list_off_24
+            )
+        ) {
+            Observer { venuesUi ->
+                viewModel.filterVenuesToAlphabeticalOrder(venuesUi)
+                imagefilterVenues.setImageResource(R.drawable.filter_list_24)
+            }
+        } else {
+            Observer { venuesUi ->
+                viewModel.clearFilterAppliedToVenues(venuesUi)
+                imagefilterVenues.setImageResource(R.drawable.filter_list_off_24)
+            }
+        }
+        viewModel.listOfVenueObservable.observe(this, venueListObserver)
+    }
+
+    private fun showVenueList() {
+        venuesAdapter = VenuesAdapter(this)
+        listOfVenues.adapter = venuesAdapter
+        listOfVenues.layoutManager = LinearLayoutManager(this)
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.fetchVenues()
+        viewModel.error.observe(this, Observer { error ->
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show()
+        })
+        viewModel.listOfVenueObservable.observe(this,
+            Observer { listOfVenues ->
+                listOfVenues?.let {
+                    venuesAdapter.setVenues(it)
+                }
+            })
+        viewModel.loading.observe(this, Observer { loading ->
+            showLoading(loading)
+        })
+    }
+
+    private fun showLoading(loading: Boolean?) {
+        if (loading == true) {
+            Toast.makeText(this, "show", Toast.LENGTH_SHORT).show()
+//            binding.progressBar.show()
+        } else {
+            Toast.makeText(this, "hide", Toast.LENGTH_SHORT).show()
+//            binding.progressBar.hide()
+        }
+    }
+
+    private fun requestInternetPermission() {
+        requestPermissions(
+            this,
+            arrayOf(Manifest.permission.INTERNET),
+            1
+        )
+    }
+
+    private fun isInternetPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.INTERNET
+        ) == PackageManager.PERMISSION_GRANTED
     }
 }
