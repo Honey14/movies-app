fda6bb271a929c2643d2c10370a6eb6ed81811cf
Honey Sonwani <honeysonwani88@gmail.com>
2023-06-29 16:52:44 +0530

    Implement `VenuesAdapter`

---
+++ feature-one/src/main/java/com/bookmyshow/feature_one/adapter/VenuesAdapter.kt
@@ -0,0 +1,74 @@
+package com.bookmyshow.feature_one.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bookmyshow.feature_one.R
+import com.bookmyshow.feature_one.model.Venue
+
+
+class VenuesAdapter(
+    private val context: Context
+) : RecyclerView.Adapter<VenuesAdapter.VenuesViewHolder>() {
+
+    val venues = mutableListOf<Venue>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VenuesViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.venue_list_item, parent, false)
+
+        return VenuesViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return venues.size
+    }
+
+    override fun onBindViewHolder(holder: VenuesViewHolder, position: Int) {
+        val venue = venues[position]
+        holder.textVenueName.text = venue.name
+        holder.textShowDate.text = venue.showDate
+        holder.textShowDate.setOnClickListener {
+            context.startActivity(Intent(context, FeatureTwoActivity::class.java))
+        }
+    }
+
+    inner class VenuesViewHolder(view: View) : ViewHolder(view) {
+        val textVenueName: TextView
+        val textShowDate: TextView
+
+        init {
+            textVenueName = view.findViewById(R.id.textVenueName)
+            textShowDate = view.findViewById(R.id.textShowDate)
+        }
+    }
+
+
+    private val venuesDiffCallback = object : DiffUtil.ItemCallback<Venue>() {
+        override fun areItemsTheSame(oldItem: Venue, newItem: Venue): Boolean {
+            val areShowTimesSame = oldItem.showtimes == newItem.showtimes
+            val isShowDateSame = oldItem.showDate == newItem.showDate
+            val isVenueSame = oldItem.name == newItem.name
+
+            return areShowTimesSame && isShowDateSame && isVenueSame
+        }
+
+        override fun areContentsTheSame(oldItem: Venue, newItem: Venue): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, venuesDiffCallback)
+
+    fun setVenues(venues: List<Venue>) {
+        differ.submitList(venues)
+    }
+}
