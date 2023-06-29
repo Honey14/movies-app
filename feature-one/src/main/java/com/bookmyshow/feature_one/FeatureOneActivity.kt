d553b23d9578cb5b090c6fd9edc23285703984c6
Honey Sonwani <honeysonwani88@gmail.com>
2023-06-29 16:51:48 +0530

    Add viewbinding in `FeatureOne` gradle module

--- feature-one/src/main/java/com/bookmyshow/feature_one/FeatureOneActivity.kt
+++ feature-one/src/main/java/com/bookmyshow/feature_one/FeatureOneActivity.kt
@@ -9,10 +9,15 @@ import com.bookmyshow.feature_one.viewmodel.FeatureOneViewModel
 class FeatureOneActivity : AppCompatActivity() {
 
     private val viewModel: FeatureOneViewModel by viewModels<FeatureOneViewModel>()
+    private lateinit var binding: ActivityFeatureOneBinding
+    private val listOfVenues
+        get() = binding.listOfVenues
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FeatureOneDaggerProvider.component.inject(this)
-        setContentView(R.layout.activity_feature_one)
+
+        binding = ActivityFeatureOneBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
