0e28ba3312aaacfcf99b695788e3f6ccdc93d088
Honey Sonwani <honeysonwani88@gmail.com>
2023-07-02 20:15:52 +0530

    Add a splash screen

--- app/src/main/java/com/bookmyshow/assignment/MainActivity.kt
+++ app/src/main/java/com/bookmyshow/assignment/MainActivity.kt
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bookmyshow.core.ImageLoader
 import com.bookmyshow.feature_one.FeatureOneActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -26,11 +30,21 @@ class MainActivity : AppCompatActivity() {
             imageUrl = "https://static.businessworld.in/article/article_extra_large_image/1609147522_O1aw88_BMS.jpg",
             imageView = requireNotNull(imageCta)
         )
+//      a better approach would have been to use the new splash screen API launched in Android 12
+//      https://developer.android.com/about/versions/12/features/splash-screen
 
-        imageCta?.setOnClickListener {
-            startActivity(
-                Intent(this, FeatureOneActivity::class.java)
-            )
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+            navigateToFeatureOne()
         }
+
+//       could not finish the splash, it would the main activity too
+//        finish()
+    }
+
+    private fun navigateToFeatureOne() {
+        startActivity(
+            Intent(this, FeatureOneActivity::class.java)
+        )
     }
 }
