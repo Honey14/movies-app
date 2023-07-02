2040ad57228af7c4258b0803066a678d54d0176e
Honey Sonwani <honeysonwani88@gmail.com>
2023-07-03 02:40:17 +0530

    Add `RxJava2CallAdapterFactory`

--- network/src/main/java/com/bookmyshow/network/provider/NetworkProviderImpl.kt
+++ network/src/main/java/com/bookmyshow/network/provider/NetworkProviderImpl.kt
@@ -3,6 +3,7 @@ package com.bookmyshow.network.provider
 import com.squareup.moshi.Moshi
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
 
@@ -27,6 +28,7 @@ internal class NetworkProviderImpl : com.bookmyshow.core.NetworkProvider {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .client(okHttpClient)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(
                 MoshiConverterFactory.create(moshi)
             )
